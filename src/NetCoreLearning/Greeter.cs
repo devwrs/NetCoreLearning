b47efc8fe9bf520ae626745ab6d443b90c0eaf55
Longoria.You <longoria.you@gmail.com>
2016-10-10 23:47:25 +0800

    add appsettings.json file and add Configuration and services into web site

---
+++ src/NetCoreLearning/Greeter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreLearning
+{
+    public interface IGreeter
+    {
+        string GetGreetString();
+    }
+    public class Greeter : IGreeter
+    {
+        private string _greeting;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _greeting = configuration["Greeting"];
+        }
+
+        public string GetGreetString()
+        {
+            return _greeting;
+        }
+    }
+}
--- src/NetCoreLearning/Startup.cs
+++ src/NetCoreLearning/Startup.cs
@@ -7,19 +7,34 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace NetCoreLearning
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            // 取得 appsettings.json 檔案裡所定義的環境變數
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(Configuration);
+            services.AddSingleton<IGreeter, Greeter>();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IGreeter greeter)
         {
             loggerFactory.AddConsole();
 
@@ -30,7 +45,8 @@ namespace NetCoreLearning
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                string message = greeter.GetGreetString();
+                await context.Response.WriteAsync(message);
             });
         }
     }
