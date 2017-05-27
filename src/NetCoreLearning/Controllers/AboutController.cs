7e1ea394e266b38a6db56780d5269b7d49f73fa2
Longoria.You <longoria.you@gmail.com>
2017-05-27 17:18:38 +0800

    add AsyncRequestTest method in About Controller

--- src/NetCoreLearning/Controllers/AboutController.cs
+++ src/NetCoreLearning/Controllers/AboutController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -30,6 +31,30 @@ namespace NetCoreLearning.Controllers
         public ContentResult Test()
         {
             return Content("This is actually a test!");
+        }
+
+        [Route("[action]")]
+        public async Task<IActionResult> AsyncRequestTest()
+        {
+            //int result = await SimplyWaitTask(2000).ConfigureAwait(false);
+            //int result = SimplyWaitTask(2000);
+            //return Content($"waiting {result} milliseconds to complete the request.");
+            int result = await AddComputationAsync(1, 2).ConfigureAwait(false);
+            return Content($"1 + 2 = {result}");
+        }
+
+        private int SimplyWaitTask(int milliSeconds)
+        {
+            //var result = Task.Delay(milliSeconds).ConfigureAwait(false);                     
+            //await result;
+            Task.Delay(milliSeconds).Wait();
+            return milliSeconds;            
+        }
+
+        private async Task<int> AddComputationAsync(int x, int y)
+        {
+            var task = Task.Run(() => x + y);            
+            return await task.ConfigureAwait(false);
         }
     }
 }
